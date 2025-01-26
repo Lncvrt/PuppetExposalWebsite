a1c0ea47a88b86176fec7ede42c478fd1d225882
Lncvrt <lncvrtreal@gmail.com>
2025-01-25 21:46:02 -0700

    Remove priority from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,25 +6,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://puppet.lncvrt.xyz/",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1,
     },
     {
       url: "https://puppet.lncvrt.xyz/youtube-archive",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.9,
     },
     {
       url: "https://puppet.lncvrt.xyz/profiles",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.8,
     },
     {
       url: "https://puppet.lncvrt.xyz/archive-info",
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7,
     },
   ];
 }
