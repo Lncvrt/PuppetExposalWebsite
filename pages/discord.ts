12e4dbc562e592fd59d66df8e425c74be9a0590f
Lncvrt <lncvrtreal@gmail.com>
2025-05-04 15:45:35 -0700

    Updated discord invite

--- pages/discord.ts
+++ pages/discord.ts
@@ -2,7 +2,7 @@ import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async () => ({
     redirect: {
-        destination: "https://discord.gg/WU3Bxn7rwp",
+        destination: "https://discord.gg/hUVVqhXwhJ",
         permanent: false, //it might be changed in the future incase of term
     },
 });
