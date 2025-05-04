0b8fef033200dc1e97ab07aa6ade852bb8e13d86
Lncvrt <lncvrtreal@gmail.com>
2025-05-03 21:18:39 -0700

    Add email and bday

--- app/page.tsx
+++ app/page.tsx
@@ -66,6 +66,8 @@ const Home = () => {
         <p><strong>New Discord:</strong> @pearxreal</p>
         <p><strong>&quot;Dads&quot; Minecraft Account</strong>: <Link draggable="false" href="https://namemc.com/profile/d96cf667-29e3-4047-977e-f2e97e031208">Natergamez</Link></p>
         <p><strong>IP:</strong> <Link href="https://ipinfo.io/73.169.161.122">73.169.161.122</Link></p>
+        <p><strong>Birthday:</strong> January 13th, 2013</p>
+        <p><strong>Email: </strong> <Link href="doggyandkira2013@gmail.com">doggyandkira2013@gmail.com</Link></p>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <button onClick={() => setShanyaDefendShow(!shanyaDefendShow)}>Click to {shanyaDefendShow ? 'hide' : 'show'} what Shanya (24 year old) wrote to defend Puppet when he said the N-Word</button>
